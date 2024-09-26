bdc0f2093cf71419f381eec481f9cc10fd910645
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-09-26 13:46:28 -0400

    Ejericio 1.16 Módulo #1  Exercise: findViewById

--- buildyourfirtsapps_1/src/main/java/com/developer/edra/buildyourfirtsapps_1/MainActivity_Module_1.kt
+++ buildyourfirtsapps_1/src/main/java/com/developer/edra/buildyourfirtsapps_1/MainActivity_Module_1.kt
@@ -1,6 +1,10 @@
 package com.developer.edra.buildyourfirtsapps_1
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,13 @@ class MainActivity_Module_1 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+
+        val testButton: Button = findViewById(R.id.btn_test)
+
+        testButton.text= "Nuevo Texto para el boton"
     }
 }
