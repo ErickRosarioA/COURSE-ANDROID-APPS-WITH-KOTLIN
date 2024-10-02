71391ce46324802d4a9524ecfda8ac0c5c9dbfde
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-10-02 14:34:27 -0400

    Modulo 2: 2.27  Exercise: Add Aligned Boxes with Click Handlers

--- colormyviews_2/src/main/java/com/developer/edra/colormyviews_2/MainActivity.kt
+++ colormyviews_2/src/main/java/com/developer/edra/colormyviews_2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.developer.edra.colormyviews_2
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,39 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(
+                findViewById(R.id.box_one_text),
+                findViewById(R.id.box_two_text),
+                findViewById(R.id.box_three_text),
+                findViewById(R.id.box_four_text),
+                findViewById(R.id.box_five_text)
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
 }
