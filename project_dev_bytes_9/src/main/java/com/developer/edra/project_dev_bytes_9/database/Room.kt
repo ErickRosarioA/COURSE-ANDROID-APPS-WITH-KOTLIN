f7d0a002b7ec2fff664f281f950c418d36a69fad
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-10-09 23:19:15 -0400

    MODULO 9: 9.8  Exercise: Add the VideoDao

--- project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/database/Room.kt
+++ project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/database/Room.kt
@@ -1,2 +1,19 @@
 
 package com.developer.edra.project_dev_bytes_9.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+@Dao
+interface VideoDao {
+    @Query("select * from databasevideo")
+    fun getVideos(): List<DatabaseVideo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+}
+
+
