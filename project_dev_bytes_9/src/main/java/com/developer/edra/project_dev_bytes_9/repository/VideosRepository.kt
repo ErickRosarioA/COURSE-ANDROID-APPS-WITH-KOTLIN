1b4b12941363896f33b97b014c7b0f2c2eaa737c
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-10-09 23:28:29 -0400

    MODULO 9: 9.14  Exercise: Build a Repository

--- project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/repository/VideosRepository.kt
+++ project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/repository/VideosRepository.kt
@@ -1,2 +1,26 @@
-
 package com.developer.edra.project_dev_bytes_9.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
+import com.developer.edra.project_dev_bytes_9.database.VideosDatabase
+import com.developer.edra.project_dev_bytes_9.database.asDomainModel
+import com.developer.edra.project_dev_bytes_9.domain.Video
+import com.developer.edra.project_dev_bytes_9.network.Network
+import com.developer.edra.project_dev_bytes_9.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+class VideosRepository(private val database: VideosDatabase) {
+    val videos: LiveData<List<Video>> =
+        database.videoDao.getVideos().map {
+            it.asDomainModel()
+        }
+
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylist().await()
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+        }
+    }
+}
