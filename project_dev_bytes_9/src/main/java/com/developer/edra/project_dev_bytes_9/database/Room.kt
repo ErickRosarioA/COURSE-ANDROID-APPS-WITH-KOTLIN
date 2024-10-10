b77b593c0cf7d246392c8d8ecf8ab513032d01a0
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-10-09 23:24:37 -0400

    MODULO 9: 9.10  Exercise: Add the VideosDatabase

--- project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/database/Room.kt
+++ project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/database/Room.kt
@@ -1,11 +1,14 @@
-
 package com.developer.edra.project_dev_bytes_9.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Database
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 
 @Dao
@@ -17,3 +20,23 @@ interface VideoDao {
     fun insertAll(vararg videos: DatabaseVideo)
 }
 
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                VideosDatabase::class.java,
+                "videos"
+            ).build()
+        }
+    }
+    return INSTANCE
+}
