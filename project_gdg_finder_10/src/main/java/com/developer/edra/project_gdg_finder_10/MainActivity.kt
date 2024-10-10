642252797d17a0e6a8d92cea5bafa96bdf4aceaf
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-10-10 01:42:20 -0400

    MODULO 10: 10.27  Exercise: Add Dark Mode Support

--- project_gdg_finder_10/src/main/java/com/developer/edra/project_gdg_finder_10/MainActivity.kt
+++ project_gdg_finder_10/src/main/java/com/developer/edra/project_gdg_finder_10/MainActivity.kt
@@ -3,6 +3,7 @@ package com.developer.edra.project_gdg_finder_10
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
@@ -20,30 +21,24 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setupNavigation()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 
-    /**
-     * Called when the hamburger menu or back button are pressed on the Toolbar
-     *
-     * Delegate this to Navigation.
-     */
+
     override fun onSupportNavigateUp() =
         navigateUp(findNavController(R.id.nav_host_fragment), binding.drawerLayout)
 
-    /**
-     * Setup Navigation for this Activity
-     */
     private fun setupNavigation() {
-        // first find the nav controller
         val navController = findNavController(R.id.nav_host_fragment)
 
         setSupportActionBar(binding.toolbar)
 
-        // then setup the action bar, tell it about the DrawerLayout
+
         setupActionBarWithNavController(navController, binding.drawerLayout)
 
 
-        // finally setup the left drawer (called a NavigationView)
+
         binding.navigationView.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
