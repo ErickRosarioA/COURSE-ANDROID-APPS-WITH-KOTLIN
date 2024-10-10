a821329fa49c25dbeefcef8ce55b20badbc309fe
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-10-09 23:37:20 -0400

    MODULO 9: 9.18  Exercise: Create a Worker

--- project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/work/RefreshDataWork.kt
+++ project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/work/RefreshDataWork.kt
@@ -1,3 +1,23 @@
-
-
 package com.developer.edra.project_dev_bytes_9.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.developer.edra.project_dev_bytes_9.database.getDatabase
+import com.developer.edra.project_dev_bytes_9.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters):
+    CoroutineWorker(appContext, params) {
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+}
