9f7d4468dd35fa2dbd2893bc985d57a443a2e08d
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-10-09 23:16:51 -0400

    MODULO 9: 9.7  Exercise: Add a DatabaseVideo Entity

--- project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/database/DatabaseEntities.kt
+++ project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/database/DatabaseEntities.kt
@@ -1,2 +1,25 @@
 package com.developer.edra.project_dev_bytes_9.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.developer.edra.project_dev_bytes_9.domain.Video
+
+@Entity
+data class DatabaseVideo constructor(
+    @PrimaryKey
+    val url: String,
+    val updated: String,
+    val title: String,
+    val description: String,
+    val thumbnail: String)
+
+fun List<DatabaseVideo>.asDomainModel(): List<Video> {
+    return map {
+        Video(
+            url = it.url,
+            title = it.title,
+            description = it.description,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
+    }
+}
--- project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/network/DataTransferObjects.kt
+++ project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/network/DataTransferObjects.kt
@@ -1,6 +1,7 @@
 
 package com.developer.edra.project_dev_bytes_9.network
 
+import com.developer.edra.project_dev_bytes_9.database.DatabaseVideo
 import com.developer.edra.project_dev_bytes_9.domain.Video
 import com.squareup.moshi.JsonClass
 
@@ -8,28 +9,35 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class NetworkVideoContainer(val videos: List<NetworkVideo>)
 
-/**
- * Videos represent a devbyte that can be played.
- */
+
 @JsonClass(generateAdapter = true)
 data class NetworkVideo(
-        val title: String,
-        val description: String,
-        val url: String,
-        val updated: String,
-        val thumbnail: String,
-        val closedCaptions: String?)
+    val title: String,
+    val description: String,
+    val url: String,
+    val updated: String,
+    val thumbnail: String,
+    val closedCaptions: String?)
 
-/**
- * Convert Network results to database objects
- */
+
 fun NetworkVideoContainer.asDomainModel(): List<Video> {
     return videos.map {
         Video(
-                title = it.title,
-                description = it.description,
-                url = it.url,
-                updated = it.updated,
-                thumbnail = it.thumbnail)
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
     }
+}
+
+fun NetworkVideoContainer.asDatabaseModel(): Array<DatabaseVideo> {
+    return videos.map {
+        DatabaseVideo(
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
+    }.toTypedArray()
 }
