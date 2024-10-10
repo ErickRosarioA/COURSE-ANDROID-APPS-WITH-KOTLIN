363d07c97923552fca2bc47518bf2ca429308abe
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-10-09 23:33:12 -0400

    MODULO 9: 9.15  Exercise: Use the Repository

--- project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/viewmodels/DevByteViewModel.kt
+++ project_dev_bytes_9/src/main/java/com/developer/edra/project_dev_bytes_9/viewmodels/DevByteViewModel.kt
@@ -1,71 +1,31 @@
-
-
 package com.developer.edra.project_dev_bytes_9.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.developer.edra.project_dev_bytes_9.domain.Video
-import com.developer.edra.project_dev_bytes_9.network.Network
-import com.developer.edra.project_dev_bytes_9.network.asDomainModel
+import com.developer.edra.project_dev_bytes_9.database.getDatabase
+import com.developer.edra.project_dev_bytes_9.repository.VideosRepository
 import kotlinx.coroutines.launch
-import java.io.IOException
 
 
 class DevByteViewModel(application: Application) : AndroidViewModel(application) {
 
-    /**
-     *
-     */
 
-    /**
-     *
-     */
+    private val database = getDatabase(application)
+    private val videosRepository = VideosRepository(database)
 
-    /**
-     * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
-     * way to set this value to observers.
-     */
-    private val _playlist = MutableLiveData<List<Video>>()
 
-    /**
-     * A playlist of videos that can be shown on the screen. Views should use this to get access
-     * to the data.
-     */
-    val playlist: LiveData<List<Video>>
-        get() = _playlist
-
-    /**
-     * init{} is called immediately when this ViewModel is created.
-     */
     init {
-        refreshDataFromNetwork()
-    }
-
-    /**
-     * Refresh data from network and pass it via LiveData. Use a coroutine launch to get to
-     * background thread.
-     */
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
-        try {
-            val playlist = Network.devbytes.getPlaylist().await()
-            _playlist.postValue(playlist.asDomainModel())
-        } catch (networkError: IOException) {
-            // Show an infinite loading spinner if the request fails
-            // challenge exercise: show an error to the user if the network request fails
+        viewModelScope.launch {
+            videosRepository.refreshVideos()
         }
     }
 
-    /**
-     */
+    val playlist = videosRepository.videos
 
-    /**
-     * Factory for constructing DevByteViewModel with parameter
-     */
+
     class Factory(val app: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(DevByteViewModel::class.java)) {
@@ -76,3 +36,4 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
         }
     }
 }
+
