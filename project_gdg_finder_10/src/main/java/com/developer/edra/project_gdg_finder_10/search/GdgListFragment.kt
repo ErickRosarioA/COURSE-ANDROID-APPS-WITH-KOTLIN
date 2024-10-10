02e6ca9f49cfcf0332e2bd38aea0953a303f3287
Erick David Rosario Alcantara <erickrosario08@outlook.com>
2024-10-10 01:36:48 -0400

    MODULO 10: 10.25  Exercise: Add a ChipGroup

--- project_gdg_finder_10/src/main/java/com/developer/edra/project_gdg_finder_10/search/GdgListFragment.kt
+++ project_gdg_finder_10/src/main/java/com/developer/edra/project_gdg_finder_10/search/GdgListFragment.kt
@@ -1,6 +1,5 @@
 package com.developer.edra.project_gdg_finder_10.search
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -12,12 +11,14 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.developer.edra.project_gdg_finder_10.R
 import com.developer.edra.project_gdg_finder_10.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 
 private const val LOCATION_PERMISSION_REQUEST = 1
@@ -37,10 +38,10 @@ class GdgListFragment : Fragment() {
     ): View {
         val binding = FragmentGdgListBinding.inflate(inflater)
 
-        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+
         binding.setLifecycleOwner(this)
 
-        // Giving the binding access to the OverviewViewModel
+
         binding.viewModel = viewModel
 
         val adapter = GdgListAdapter(GdgClickListener { chapter ->
@@ -48,7 +49,7 @@ class GdgListFragment : Fragment() {
             startActivity(Intent(Intent.ACTION_VIEW, destination))
         })
 
-        // Sets the adapter of the RecyclerView
+
         binding.gdgChapterList.adapter = adapter
 
         viewModel.showNeedLocation.observe(viewLifecycleOwner, object : Observer<Boolean> {
@@ -60,6 +61,29 @@ class GdgListFragment : Fragment() {
                         "No location. Enable location in settings (hint: test with Maps) then check app permissions!",
                         Snackbar.LENGTH_LONG
                     ).show()
+                }
+            }
+        })
+
+        viewModel.regionList.observe(viewLifecycleOwner, object : Observer<List<String>> {
+            override fun onChanged(value: List<String>) {
+                val chipGroup = binding.regionList
+                val inflator = LayoutInflater.from(chipGroup.context)
+
+                val children = value.map { regionName ->
+                    val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+                    chip.text = regionName
+                    chip.tag = regionName
+                    chip.setOnCheckedChangeListener { button, isChecked ->
+                        viewModel.onFilterChanged(button.tag as String, isChecked)
+                    }
+                    chip
+                }
+
+                chipGroup.removeAllViews()
+
+                for (chip in children) {
+                    chipGroup.addView(chip)
                 }
             }
         })
@@ -80,9 +104,8 @@ class GdgListFragment : Fragment() {
     }
 
 
-    @SuppressLint("MissingPermission")
     private fun requestLastLocationOrStartLocationUpdates() {
-        // if we don't have permission ask for it and wait until the user grants it
+
         if (ContextCompat.checkSelfPermission(
                 requireContext(),
                 LOCATION_PERMISSION
@@ -103,10 +126,8 @@ class GdgListFragment : Fragment() {
         }
     }
 
-
-    @SuppressLint("MissingPermission")
     private fun startLocationUpdates(fusedLocationClient: FusedLocationProviderClient) {
-        // if we don't have permission ask for it and wait until the user grants it
+
         if (ContextCompat.checkSelfPermission(
                 requireContext(),
                 LOCATION_PERMISSION
